ec8aeab1254982c1097d32ccc900e9a60620e275
HeeyeonCho <whgmldus819@gmail.com>
2021-02-05 23:45:59 +0900

    구글로그인+아웃+탈퇴_test.v_HY

--- app/src/main/java/org/Photy/photy/MainActivity.kt
+++ app/src/main/java/org/Photy/photy/MainActivity.kt
@@ -2,10 +2,40 @@ package org.Photy.photy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    lateinit var btn_logout:Button
+    lateinit var btn_revoke: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_logout = findViewById(R.id.btn_logout)
+        btn_revoke = findViewById(R.id.btn_revoke)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // 로그아웃 버튼
+        btn_logout.setOnClickListener{
+            signOut()
+        }
+
+        // 회원탈퇴 버튼
+        btn_revoke.setOnClickListener {
+            revokeAccess()
+        }
+    }
+
+    private fun signOut(){//로그아웃
+        firebaseAuth.signOut()
+    }
+
+    private fun revokeAccess() { //회원탈퇴
+        firebaseAuth.currentUser?.delete()
     }
 }
