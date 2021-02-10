61f8c54ff6db38c9029b853d4f7a8da21844ed30
Jeong subin <eun05178@naver.com>
2021-02-11 01:11:38 +0900

    + 튜토리얼 화면 완성

--- app/src/main/java/org/Photy/photy/SplashActivity.kt
+++ app/src/main/java/org/Photy/photy/SplashActivity.kt
@@ -4,26 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.os.Build
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // making the status bar transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
+            window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-            finish()
-        },DURATION)
-
-    }
-    companion object {
-        private const val DURATION : Long = 3000
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
+        Handler().postDelayed(object: Runnable {
+            override fun run() {
+                startActivity(Intent(this@SplashActivity, IntroSliderActivity::class.java))
+                finish()
+            }
+        }, 2000)
     }
 }
