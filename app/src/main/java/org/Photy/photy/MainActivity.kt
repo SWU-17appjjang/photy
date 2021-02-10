5714ddd5b90ccae2443b0892d61c297da3d510f3
yelynAhn <yelynahn@gmail.com>
2021-02-11 01:51:22 +0900

    로그아웃. 회원 탈퇴 구현 완성, 뒤로가기로 앱 종료 기능 추가, activity_nevigation_aboutus.xml(미완) activity_nevigation_withdrawal.xml(완성) 추가

--- app/src/main/java/org/Photy/photy/MainActivity.kt
+++ app/src/main/java/org/Photy/photy/MainActivity.kt
@@ -22,7 +22,9 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
@@ -31,11 +33,13 @@ import com.gun0912.tedpermission.TedPermission
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_nevigation_withdrawal.*
 import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.android.synthetic.main.activity_upload.*
 import kotlinx.android.synthetic.main.main_toolbar.*
 import java.io.File
 import java.io.IOException
+import java.lang.System.currentTimeMillis
 import java.nio.channels.AsynchronousFileChannel.open
 import java.nio.channels.DatagramChannel.open
 import java.text.SimpleDateFormat
@@ -45,8 +49,6 @@ import kotlin.jvm.Throws
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var firebaseAuth: FirebaseAuth
-    lateinit var btn_logout:Button
-    lateinit var btn_revoke: Button
 
     val REQUEST_IMAGE_CAPTURE = 1
     lateinit var currentPhotoPath : String
@@ -55,6 +57,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     lateinit var btn_upload: Button
     var fbStorage: FirebaseStorage? = null
+
+    // 이전 클릭 시간 (뒤로가기를 위한)
+    private var lastTimeBackPressed:Long=-1500
+
+    private lateinit var activity: MainActivity
+    private lateinit var googleSignInClient: GoogleSignInClient
+
 
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
@@ -67,22 +76,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         main_navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
 
-        /*
-        btn_logout = findViewById(R.id.btn_logout)
-        btn_revoke = findViewById(R.id.btn_revoke)
-
-        //firebaseAuth = FirebaseAuth.getInstance()
-
-        // 로그아웃 버튼
-        btn_logout.setOnClickListener{
-            signOut()
-        }
-
-        // 회원탈퇴 버튼
-        btn_revoke.setOnClickListener {
-            revokeAccess()
-        }
-         */
         settingPermission() // 카메라 권한체크 시작
 
         btn_picture=findViewById(R.id.btn_picture) //사진 찍기 버튼
@@ -96,13 +89,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         var permis = object  : PermissionListener {
             //            어떠한 형식을 상속받는 익명 클래스의 객체를 생성하기 위해 다음과 같이 작성
             override fun onPermissionGranted() {
-                Toast.makeText(this@MainActivity, "권한 허가", Toast.LENGTH_SHORT)
-                    .show()
             }
 
             override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                Toast.makeText(this@MainActivity, "권한 거부", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(this@MainActivity, "권한 거부", Toast.LENGTH_SHORT).show()
                 ActivityCompat.finishAffinity(this@MainActivity) // 권한 거부시 앱 종료
             }
         }
@@ -186,7 +176,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         }
 
-
         btn_upload =findViewById(R.id.btn_upload)
 
         // 크롭한 이미지를 이미지 뷰에 나타냄
@@ -200,12 +189,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     // 업로드 버튼 클릭시 Firebase에 사진 업로드
                     btn_upload.setOnClickListener {
                         funImageUpload(result.uri)
-                        setContentView(R.layout.main_layout)
-                    }
 
+                    }
                     // 홈버튼 클릭시 main 레이아웃으로 회귀
-                    btn_home.setOnClickListener {
-                        setContentView(R.layout.main_layout)
+                    btn_home.setOnClickListener{
+                        var intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
                     }
                 }
             } else if (resultCode === CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
@@ -225,6 +214,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         storageRef?.putFile(uriPhoto!!)?.addOnSuccessListener {
             Toast.makeText(img_picture.context, "이미지를 업로드했습니다!", Toast.LENGTH_SHORT).show()
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
         storageRef?.putFile(uriPhoto!!)?.addOnFailureListener{
             Toast.makeText(img_picture.context, "이미지를 업로드하지 못했습니다!", Toast.LENGTH_SHORT).show()
@@ -233,7 +224,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // 상단 메뉴 클릭시
         when(item.itemId){
             android.R.id.home->{ // 메뉴 버튼
                 main_drawer_layout.openDrawer(GravityCompat.START)    // 네비게이션 드로어 열기
@@ -243,9 +234,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean { // 메뉴 바 버튼 클릭 시
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
         when(item.itemId){
-            R.id.signout-> Toast.makeText(this,"기능 구현 예정",Toast.LENGTH_SHORT).show()
-            R.id.logout-> Toast.makeText(this,"기능 구현 예정",Toast.LENGTH_SHORT).show()
+            R.id.withdrawal-> revokeAccess()
+            R.id.logout-> signOut()
             R.id.maker-> Toast.makeText(this,"음하하 우리가 만들었지",Toast.LENGTH_SHORT).show()
         }
         return false
@@ -257,17 +257,55 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             // 테스트를 위해 뒤로가기 버튼시 Toast 메시지
             Toast.makeText(this,"back btn clicked",Toast.LENGTH_SHORT).show()
         } else{
-            super.onBackPressed()
+            // (현재 버튼 누른 시간-이전에 버튼 누른 시간) <=1.5초일 때 동작
+            if(currentTimeMillis() -lastTimeBackPressed<=1500)
+                finish()
+            lastTimeBackPressed= currentTimeMillis()
+            Toast.makeText(this,"이전 버튼을 한 번 더 누르면 종료됩니다",Toast.LENGTH_SHORT).show()
         }
-
     }
 
-    private fun signOut(){//로그아웃
+
+    private fun signOut(){ // 로그아웃
         firebaseAuth.signOut()
+        Toast.makeText(this,"로그아웃 하셨습니다.",Toast.LENGTH_SHORT).show()
+        var intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
     }
 
-    private fun revokeAccess() { //회원탈퇴
-        firebaseAuth.currentUser?.delete()
+    private fun revokeAccess() { // 회원탈퇴
+        setContentView(R.layout.activity_nevigation_withdrawal) //signout 레이아웃으로 변경
+
+        // no 버튼 클릭시 메인 화면으로 회귀
+        btn_no.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        // 탈퇴 클릭시 탈퇴 처리 후 로그인 화면으로 회귀
+        btn_withdrawal.setOnClickListener{
+            firebaseAuth.currentUser?.delete()
+            Toast.makeText(this,"회원탈퇴 하셨습니다.",Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
+
+    /*
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        //재로그인 시 자동로그인 방지
+        googleSignInClient.signOut().addOnCompleteListener {
+            activity.finish()
+        }
+    }
+     */
 
 }
